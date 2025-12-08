bd5eceac25aefb03b2bb4e5e406acb1551f254fd
danielmcintyre14 <184821430+danmcintyre14@users.noreply.github.com>
2025-12-08 18:50:58 +0400

    added additional videos

--- src/data/data.js
+++ src/data/data.js
@@ -11,4 +11,19 @@ export const videoData = [
         description: 'Basic exercise technique and common faults',
         youtubeId: "krCyp67d1AM"
     },
+    {
+        title: 'Chest Press Plate Loaded',
+        description: 'Basic exercise technique and common faults',
+        youtubeId: "C7bI48vXHP8"
+    },
+     {
+        title: 'Seated Row Pin Loaded',
+        description: 'Basic exercise technique and common faults',
+        youtubeId: "l751cwjQPq4"
+    },
+     {
+        title: 'Shoulder Press Plate Loaded',
+        description: 'Basic exercise technique and common faults',
+        youtubeId: "kzQifoVGAOo"
+    }
 ]
